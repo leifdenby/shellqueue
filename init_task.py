91c2794c3f076430c0178be54344ffcaa72b0b60
Leif Denby <leif@denby.eu>
2014-07-28 16:18:20 +0100

    Better task name handling and enqueue cmd

--- init_task.py
+++ init_task.py
@@ -15,18 +15,31 @@ source_directory = sys.argv[1]
 target_base_directory = sys.argv[2]
 project_name = os.path.basename(source_directory)
 
-target_directory = os.path.join(target_base_directory, 'planning', project_name)
-if os.path.exists(target_directory):
-    n = 0
-    while True:
-        if len(project_name.split('-')) > 1 and project_name.split('-')[-1].isdigit():
-            project_name = '-'.join(project_name.split('-')[:-1])
-        target_directory = os.path.join(target_base_directory, "planning", "%s-%d" % (project_name, n))
+def project_exists(name):
+    for tf in ['planning', 'scheduled', 'completed', 'processing']:
+        target_directory = os.path.join(target_base_directory, tf, project_name)
         if os.path.exists(target_directory):
-            n += 1
+            return True
+    else:
+        return False
+
+def make_next_name(project_name):
+    if len(project_name.split('-')) > 1 and project_name.split('-')[-1].isdigit():
+        num = int(project_name.split('-')[-1]) + 1
+        project_name = '-'.join(project_name.split('-')[:-1] + [str(num),])
+    else:
+        project_name = project_name + '-0'
+    return project_name
+
+if project_exists(project_name):
+    while True:
+        project_name = make_next_name(project_name)
+        if project_exists(project_name):
+            pass
         else:
             break
 
+target_directory = os.path.join(target_base_directory, 'planning', project_name)
 os.makedirs(target_directory)
 
 manifest_s_filename = os.path.join(source_directory, 'shellqueue.manifest')
